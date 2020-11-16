732d99993d142f89d54aa41a1ace7bb2af18428a
Ivan Lazarevski <Ivan.Lazarevski@seavus.com>
2020-11-16 09:33:59 +0100

    config vars and env setup

--- setup.js
+++ setup.js
@@ -6,12 +6,14 @@ const addToEnvironment = (json) => {
   }
 };
 
-// [Firebase]
-let fbFile = fs.readFileSync("./firebase.js").toString();
-fbFile = fbFile.substr(21, fbFile.length - 25);
-addToEnvironment(JSON.parse(fbFile));
+if (process.env.ENVIRONMENT == "dev") {
+  // [Firebase]
+  let fbFile = fs.readFileSync("./firebase.js").toString();
+  fbFile = fbFile.substr(21, fbFile.length - 25);
+  addToEnvironment(JSON.parse(fbFile));
 
-// [Twitter]
-let twFile = fs.readFileSync("./twitter.js").toString();
-twFile = twFile.substr(20, twFile.length - 24);
-addToEnvironment(JSON.parse(twFile));
+  // [Twitter]
+  let twFile = fs.readFileSync("./twitter.js").toString();
+  twFile = twFile.substr(20, twFile.length - 24);
+  addToEnvironment(JSON.parse(twFile));
+}
